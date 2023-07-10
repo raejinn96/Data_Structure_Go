095d277ef437301dbee21b7e564d261d5ea4ee91
raejinn96 <smlekmew@gmail.com>
2023-07-10 17:51:50 +0700

    Revisi fungsi untuk ArrayBaru

--- Penggabungan/main.go
+++ Penggabungan/main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+func main() {
+
+	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
+
+	fmt.Println(ArrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
+
+	fmt.Println(ArrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
+
+	fmt.Println(ArrayMerge([]string{}, []string{"devil jin", "sergei"}))
+
+	fmt.Println(ArrayMerge([]string{"hwoarang"}, []string{}))
+
+	fmt.Println(ArrayMerge([]string{}, []string{}))
+
+}
+
+func cek(slice []string, elemen string) bool {
+	for _, item := range slice {
+		if item == elemen {
+			return true
+		}
+	}
+	return false
+}
+
 func ArrayMerge(ArrayA, ArrayB []string) []string {
 
 	ArrayBaru := make([]string, 0)
@@ -19,29 +44,4 @@ func ArrayMerge(ArrayA, ArrayB []string) []string {
 	}
 
 	return ArrayBaru
-}
-
-func cek(slice []string, elemen string) bool {
-	for _, item := range slice {
-		if item == elemen {
-			return true
-		}
-	}
-	return false
-}
-
-func main() {
-
-	fmt.Println(ArrayMerge([]string{"king", "devil jin", "akuma"}, []string{"eddie", "steve", "geese"}))
-
-	fmt.Println(ArrayMerge([]string{"sergei", "jin"}, []string{"jin", "steve", "bryan"}))
-
-	fmt.Println(ArrayMerge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
-
-	fmt.Println(ArrayMerge([]string{}, []string{"devil jin", "sergei"}))
-
-	fmt.Println(ArrayMerge([]string{"hwoarang"}, []string{}))
-
-	fmt.Println(ArrayMerge([]string{}, []string{}))
-
 }
